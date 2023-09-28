604bedc051fb50f42aee9413b225ce7fb8f810b6
catcodey <103652349+catcodey@users.noreply.github.com>
2023-09-28 17:33:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,20 @@ import matplotlib.pyplot as plt
 import numpy as np
 import datetime as dt
 import io
+import subprocess
 
+# List of required libraries
+required_libraries = ["numpy", "pandas", "matplotlib"]
+
+# Check if the required libraries are installed
+missing_libraries = [lib for lib in required_libraries if not st.subprocess.call(['pip', 'show', lib]) == 0]
+
+# Install missing libraries if any
+if missing_libraries:
+    st.write("Installing missing libraries...")
+    for lib in missing_libraries:
+        st.subprocess.call(['pip', 'install', lib])
+    st.write("Libraries installed successfully!"
 with st.sidebar:
   selected=option_menu(
     menu_title=None,
