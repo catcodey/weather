ec86863467c7fbebb6623a9daef20f0f7ffadc7d
catcodey <103652349+catcodey@users.noreply.github.com>
2023-09-28 19:44:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,12 @@
 import streamlit as st
-
+import pandas as pd
 
 import seaborn as sns
 
 st.title("My Streamlit App")
 
+df=pd.read_csv("air-quality-india")
+st.write(df)
 # List of required libraries
 
 
